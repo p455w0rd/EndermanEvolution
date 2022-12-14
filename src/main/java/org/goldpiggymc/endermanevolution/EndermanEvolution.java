95b0b296941c3413a5a54eba4402465c7c63c60c
TobyDev0207 <118341149+TobyDev0207@users.noreply.github.com>
2022-12-14 20:01:21 +0700

    Fabric!

---
+++ src/main/java/org/goldpiggymc/endermanevolution/EndermanEvolution.java
@@ -0,0 +1,11 @@
+package org.goldpiggymc.endermanevolution;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EndermanEvolution implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
