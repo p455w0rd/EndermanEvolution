38d999a8a1fa547025587bd949b912603f938f01
TobyDev0207 <118341149+TobyDev0207@users.noreply.github.com>
2022-12-31 23:58:40 +0700

    Use OwoItemSettings instead of FabricItemSettings

--- src/main/java/org/goldpiggymc/endermanevolution/ItemManager.java
+++ src/main/java/org/goldpiggymc/endermanevolution/ItemManager.java
@@ -1,10 +1,11 @@
 package org.goldpiggymc.endermanevolution;
 
+import io.wispforest.owo.itemgroup.OwoItemSettings;
 import io.wispforest.owo.registration.reflect.ItemRegistryContainer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 
 public class ItemManager implements ItemRegistryContainer {
-    public static final Item FRIENDER_PEARL = new Item(new FabricItemSettings().group(Vars.ITEM_GROUP));
-    public static final Item ENDER_FRAGMENT = new Item(new FabricItemSettings().group(Vars.ITEM_GROUP));
+    public static final Item FRIENDER_PEARL = new Item(new OwoItemSettings().group(Vars.ITEM_GROUP));
+    public static final Item ENDER_FRAGMENT = new Item(new OwoItemSettings().group(Vars.ITEM_GROUP));
 }
