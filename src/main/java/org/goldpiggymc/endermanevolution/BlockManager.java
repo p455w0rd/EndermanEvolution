9a4b053aa237fea3f41838d280935d4b68828392
TobyDev0207 <118341149+TobyDev0207@users.noreply.github.com>
2023-01-16 23:35:05 +0700

    Rename blocks to block

--- src/main/java/org/goldpiggymc/endermanevolution/BlockManager.java
+++ src/main/java/org/goldpiggymc/endermanevolution/BlockManager.java
@@ -3,10 +3,9 @@ package org.goldpiggymc.endermanevolution;
 import io.wispforest.owo.registration.reflect.BlockRegistryContainer;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.CropBlock;
 import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
-import org.goldpiggymc.endermanevolution.blocks.EnderFlowerCrop;
+import org.goldpiggymc.endermanevolution.block.EnderFlowerCrop;
 
 public class BlockManager implements BlockRegistryContainer {
     public static final Block ENDER_FLOWER = new EnderFlowerCrop(AbstractBlock.Settings.of(Material.PLANT).nonOpaque().noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP));
--- src/main/java/org/goldpiggymc/endermanevolution/block/EnderFlowerCrop.java
+++ src/main/java/org/goldpiggymc/endermanevolution/block/EnderFlowerCrop.java
@@ -1,12 +1,10 @@
-package org.goldpiggymc.endermanevolution.blocks;
+package org.goldpiggymc.endermanevolution.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.item.ItemConvertible;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
